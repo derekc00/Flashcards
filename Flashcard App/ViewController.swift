e2fc48fe06f1df574b21e360e5cca5b40cff13ce
Derek Chang <derektc1@uci.edu>
2020-02-27 23:32:52 -0800

    Initial Commit

---
+++ Flashcard App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcard App
+//
+//  Created by Derek Chang on 2/27/20.
+//  Copyright © 2020 Derek Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
